a1b6c7ffc8f53ebd9e3bd8acba99d85d75172299
krpopp <karina.popp@nyu.edu>
2017-06-28 18:39:11 +0200

    Code review + comments

--- assets/scripts/Manager.js
+++ assets/scripts/Manager.js
@@ -23,19 +23,19 @@ SceneGame.Manager.prototype = {
 
     create: function () {
         var manager = this;
+        //set up variables
         manager.cursors = manager.input.keyboard.createCursorKeys();
         manager.scenesJSON = manager.cache.getJSON('scenes');
-        manager.keySet = [];
         manager.preloadedSets = [];
         manager.unloadedSets = [];
+
+        //seperate init loaded scenes from unloaded ones
         for (var i = 0; i < manager.scenesJSON.Scenes.length; i++) {
             if (manager.scenesJSON.Scenes[i].time == 'morning') {
                 manager.preloadedSets.push(manager.scenesJSON.Scenes[i].name);
             } else if (manager.scenesJSON.Scenes[i].time == 'start') {
-                manager.preloadedSets.push(manager.scenesJSON.Scenes[i].name);
                 manager.currentScene = manager.scenesJSON.Scenes[i];
-                var getSheets = manager.scenesJSON.Scenes[i].sheets;
-                var getName = manager.scenesJSON.Scenes[i].name;
+                manager.preloadedSets.push(manager.currentScene.name);
                 var getKeys = [];
                 for (var j = 0; j < manager.scenesJSON.Scenes[i].keySet.length; j++) {
                     var tempVar = eval(manager.scenesJSON.Scenes[i].keySet[j]);
@@ -45,34 +45,45 @@ SceneGame.Manager.prototype = {
                 manager.unloadedSets.push(manager.scenesJSON.Scenes[i].name);
             }
         }
-        manager.spriteSet = [];
-        manager.sheetSet = [];
-        manager.sceneSpeed = manager.currentScene.speed;
-        manager.keySet = [];
 
+        //also, set the keys for the initial scene
+        //for some reason, this one can't be set by the function
         manager.keySet = getKeys;
-        console.log("the create key set is " + manager.keySet);
-        manager.PhotoLoader(getSheets, getName);
+
+        //load photos, set up the next scene
+        manager.PhotoLoader();
 
     },
 
-    PhotoLoader: function (count, name) {
+    //this grabs and binds the keys for each level
+    FindKeys: function () {
         var manager = this;
-        manager.count = count;
-        manager.name = name;
-        for (var i = 0; i < count; i++) {
-            manager.sheetSet.push(name + '-' + i);
-            for (var j = 0; j < 4; j++) {
-                manager.spriteSet.push(name + i + j);
+        var getKeys = [];
+        manager.keySet = [];
+        if (manager.currentScene.keySet === Array) {
+            for (var j = 0; j < manager.currentScene.keySet.length; j++) {
+                var tempVar = eval(manager.currentScene.keySet[j]);
+                getKeys.push(tempVar);
             }
+        } else {
+            getKeys.push(eval(manager.currentScene.keySet))
         }
+        manager.keySet = getKeys;
+    },
+
+    //sets some parameters for the scene
+    //preloads a scene five units away
+    PhotoLoader: function () {
+        var manager = this;
+        manager.sceneSpeed = manager.currentScene.speed;
         if (manager.unloadedSets.length != 0) {
-            var nextSet = manager.unloadedSets[Math.floor(Math.random() * manager.unloadedSets.length)];
-            for (var i = 0; i < this.scenesJSON.Scenes.length; i++) {
-                if (this.scenesJSON.Scenes[i].name == nextSet) {
-                    var sheetNum = this.scenesJSON.Scenes[i].sheets;
-                    for (var j = 0; j < sheetNum; j++) {
-                        this.load.atlasJSONArray(this.scenesJSON.Scenes[i].name + '-' + j, 'assets/textures/' + this.scenesJSON.Scenes[i].name + '-' + j + '.png', 'assets/textures/' + this.scenesJSON.Scenes[i].name + '-' + j + '.json');
+            var nextSet = manager.unloadedSets.pop();
+            manager.preloadedSets.push(nextSet);
+            for (var i = 0; i < manager.scenesJSON.Scenes.length; i++) {
+                if (manager.scenesJSON.Scenes[i].name == nextSet) {
+                    var sheetNum = manager.scenesJSON.Scenes[i].sheets;
+                    for (var j = 0; j < manager; j++) {
+                        this.load.atlasJSONArray(manager.scenesJSON.Scenes[i].name + '-' + j, 'assets/textures/' + manager.scenesJSON.Scenes[i].name + '-' + j + '.png', 'assets/textures/' + manager.scenesJSON.Scenes[i].name + '-' + j + '.json');
                     }
                 }
             }
@@ -85,9 +96,11 @@ SceneGame.Manager.prototype = {
 
     },
 
+    //creates the actual sprites for the level
+    //should perhaps run these in reverse, so that the levels are loading while play is happening
     PhotoCreate: function () {
         var manager = this;
-        manager.photo = manager.add.sprite(0, 0, manager.name + '-' + '0', manager.name + '00');
+        manager.photo = manager.add.sprite(0, 0, manager.currentScene.name + '-' + '0', manager.currentScene.name + '00');
         manager.keysPressed = [];
         for (var i = 0; i < manager.keySet.length; i++) {
             manager.keysPressed[i] = false;
@@ -100,6 +113,7 @@ SceneGame.Manager.prototype = {
         manager.gameReady = true;
     },
 
+    //picks the next scene from the array of already loaded scenes
     NextScene: function () {
         var manager = this;
         manager.world.removeAll();
@@ -108,24 +122,8 @@ SceneGame.Manager.prototype = {
             for (var i = 0; i < this.scenesJSON.Scenes.length; i++) {
                 if (this.scenesJSON.Scenes[i].name == nextSet) {
                     manager.currentScene = manager.scenesJSON.Scenes[i];
-
-                    var getKeys = [];
-                    console.log(manager.currentScene.name);
-                    if (manager.currentScene.keySet === Array) {
-                        for (var j = 0; j < manager.currentScene.keySet.length; j++) {
-                            var tempVar = eval(manager.currentScene.keySet[j]);
-                            getKeys.push(tempVar);
-                        }
-                    } else {
-                        getKeys.push(eval(manager.currentScene.keySet))
-                    }
-
-                    manager.keySet = [];
-
-                    manager.keySet = getKeys;
-                    console.log("the key set in the enxt scene is " + manager.keySet);
-                    manager.sceneSpeed = manager.currentScene.speed;
-                    manager.PhotoLoader(manager.currentScene.sheets, manager.currentScene.name);
+                    manager.FindKeys();
+                    manager.PhotoLoader();
                     break;
                 }
             }
@@ -135,7 +133,7 @@ SceneGame.Manager.prototype = {
         }
     },
 
-
+    //here is where i'm running the code to detect input
     update: function () {
         var manager = this;
         if (manager.gameReady) {
@@ -145,20 +143,22 @@ SceneGame.Manager.prototype = {
                     if (manager.spriteNum >= 3) {
                         manager.sheetNum += 1;
                         manager.spriteNum = 0;
-                        if (manager.sheetNum >= manager.sheetSet.length) {
+                        if (manager.sheetNum >= manager.currentScene.sheets) {
                             manager.gameReady = false;
                         } else {
-                            var tempSprite = manager.add.sprite(0, 0, manager.name + '-' + manager.sheetNum, manager.name + manager.sheetNum + manager.spriteNum);
+                            var tempSprite = manager.add.sprite(0, 0, manager.currentScene.name + '-' + manager.sheetNum, manager.currentScene.name + manager.sheetNum + manager.spriteNum);
                         }
                     } else {
                         manager.spriteNum += 1;
-                        var tempSprite = manager.add.sprite(0, 0, manager.name + '-' + manager.sheetNum, manager.name + manager.sheetNum + manager.spriteNum);
+                        var tempSprite = manager.add.sprite(0, 0, manager.currentScene.name + '-' + manager.sheetNum, manager.currentScene.name + manager.sheetNum + manager.spriteNum);
                     }
                 }
             }
         }
     },
 
+    //case statment to find out which function to run
+    //for different kinds of input patterns
     KeyCheckSwitch: function (pattern) {
         var manager = this;
         switch (pattern) {
@@ -180,10 +180,13 @@ SceneGame.Manager.prototype = {
         }
     },
 
+    ////FROM HERE ON I NEED TO FIX AND COMPLETE THIS CODE
+
+
+    //for holding down one+ keys
     HoldKeys: function () {
         var manager = this;
         if (manager.keySet[0].isDown) {
-            console.log("press");
             manager.keysPressed[0] = true;
         } else {
             manager.keysPressed[0] = false;
@@ -220,11 +223,14 @@ SceneGame.Manager.prototype = {
         }
     },
 
+    //move this
+    //checks if every bool in an array is true
     AreTrue: function (element, index, array) {
         var manager = this;
         return element == true;
     },
 
+    //for when keys must be pressed in a certain order
     SequentialKeys: function () {
         var manager = this;
         var keyOnePress = false;
@@ -253,11 +259,13 @@ SceneGame.Manager.prototype = {
         }
     },
 
+    //for repeatedly tapping a single key
     TapKeys: function () {
         var manager = this;
-        //manager.tap = manager.keySet[0].onDown.add(manager.IncreaseInt, this);
+        manager.tap = manager.keySet[0].onDown.add(manager.IncreaseInt, this);
     },
 
+    //for when you can press any key :p 
     AnyKey: function () {
         var manager = this;
         manager.input.keyboard.onDownCallback = function () {
@@ -265,6 +273,8 @@ SceneGame.Manager.prototype = {
         }
     },
 
+    //going back and forth between two keys
+    //can possibl combine with or make distinct from SequentialKeys
     SwitchKeys: function () {
         var manager = this;
         var keyOnePress = false;
@@ -277,8 +287,10 @@ SceneGame.Manager.prototype = {
         }
     },
 
+    //Int that increases based on input in order to move the photos forward
     IncreaseInt: function () {
         var manager = this;
+        console.log("int increased");
         manager.upInt += 1;
 
     }
